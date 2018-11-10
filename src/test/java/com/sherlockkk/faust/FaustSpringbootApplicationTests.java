b708472b2b84852e926966157c1b26a6698bcc5d
SongJian <songjian0x00@163.com>
2018-11-10 18:19:01 +0800

    日志配置测试
    Signed-off-by: SongJian <songjian0x00@163.com>

--- src/test/java/com/sherlockkk/faust/FaustSpringbootApplicationTests.java
+++ src/test/java/com/sherlockkk/faust/FaustSpringbootApplicationTests.java
@@ -1,5 +1,6 @@
 package com.sherlockkk.faust;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,10 +8,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class FaustSpringbootApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+
+    @Test
+    public void logTest() {
+        String user = "啊啊啊";
+        String password = "password";
+        log.info("user:{}, password:{}", user, password);
+        log.error("user:{}, password:{}", user, password);
+
     }
 
 }
