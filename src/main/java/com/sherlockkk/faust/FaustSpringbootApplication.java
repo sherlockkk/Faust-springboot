4c93a7a96991345feba0b40ad027dfae4bcabb4b
SongJian <songjian0x00@163.com>
2018-11-10 17:19:42 +0800

    项目初始化
    Signed-off-by: SongJian <songjian0x00@163.com>

---
+++ src/main/java/com/sherlockkk/faust/FaustSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.sherlockkk.faust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaustSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaustSpringbootApplication.class, args);
+    }
+}
